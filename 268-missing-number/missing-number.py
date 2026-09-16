46ff2fddffcdce6a37dc65822ee6182279818759
Ansh Choudhary <anshchoudhary176@gmail.com>
2026-09-16 12:08:55 +0530

    Time: 45 ms (16.54%) | Memory: 20.4 MB (75.84%) - LeetSync

---
+++ 268-missing-number/missing-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def missingNumber(self, nums: list[int]) -> int:
+        
+        n = len(nums)
+        ans = n
+
+        for i in range(n):
+            ans ^= i
+            print(ans)
+            ans ^= nums[i]
+            print(ans)
+        return ans
